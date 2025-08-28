39df678021b3f0d6310f2f4ff77bb997afd49c9c
Nick Joyce <actan-spb@mail.ru>
2025-08-28 14:41:28 +0300

    -

--- app/tasks/monitoring.py
+++ app/tasks/monitoring.py
@@ -6,7 +6,7 @@ logger = logging.getLogger(__name__)
 
 
 @celery_app.task()
-def tg_channel(self):
+def tg_channel():
     logger.info("Task started")
     time.sleep(2)
     logger.info("Task completed")
