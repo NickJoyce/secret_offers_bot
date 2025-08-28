ef467cb4c9d1028ca21d7f64ff4b53e56c4fd248
Nick Joyce <actan-spb@mail.ru>
2025-08-28 14:28:28 +0300

    -

--- app/celery.py
+++ app/celery.py
@@ -23,7 +23,7 @@ celery_app.conf.update(
 
 celery_app.conf.beat_schedule = {
     "tg_channel_monitoring": {
-        "task": "app.tasks.monitoring.tg_channel",
+        "task": "celery_app.tasks.monitoring.tg_channel",
         "schedule": crontab(minute="*/1")
     }
 }
