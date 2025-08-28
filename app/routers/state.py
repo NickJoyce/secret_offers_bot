c1332cfdb6ae000129aa9dfbbe7d83e3300f87af
Nick Joyce <actan-spb@mail.ru>
2025-08-28 17:18:10 +0300

    -

--- app/routers/state.py
+++ app/routers/state.py
@@ -5,7 +5,7 @@ import logging
 from fastapi.templating import Jinja2Templates
 from settings.base import TEMPLATES_DIR
 from app.tasks.monitoring import tg_channel
-
+import traceback
 
 
 logger = logging.getLogger(__name__)
@@ -17,6 +17,12 @@ templates = Jinja2Templates(directory=TEMPLATES_DIR)
 
 @router.get("/state", include_in_schema=False)
 async def health_check(request: Request):
-    user_channel_status = await bot.get_chat_member(chat_id='-1002525082412', user_id='520704135')
-    logger.info(f"user_channel_status: {user_channel_status}")
-    return {"user_channel_status": str(user_channel_status)}
+    try:
+        user_channel_status = await bot.get_chat_member(chat_id='-1002525082412', user_id='520704135')
+        logger.info(f"user_channel_status: {user_channel_status}")
+        return str(user_channel_status)
+    except Exception as e:
+        logger.error(f"Error: {traceback.format_exc()}")
+        return str(traceback.format_exc())
+
+    
