1754d66f2ad1deb842f84d324248c1442949f7bd
Nick Joyce <actan-spb@mail.ru>
2025-08-28 13:41:06 +0300

    -

--- app/celery.py
+++ app/celery.py
@@ -4,6 +4,7 @@ from celery import Celery
 celery_app = Celery("celery_worker", 
                     broker_url="redis://127.0.0.1:6379/0", 
                     result_backend="redis://127.0.0.1:6379/0",)
+
 celery_app.conf.update(
     task_serializer='json',
     result_serializer='json',
@@ -13,6 +14,7 @@ celery_app.conf.update(
     broker_connection_retry_on_startup=True,
     task_acks_late=True,
     task_reject_on_worker_lost=True,
+    imports=("tasks.monitoring",)
 )
 
 celery_app.autodiscover_tasks()
--- tasks/monitoring.py
+++ tasks/monitoring.py
@@ -1,8 +1,8 @@
-# from app.main import celery_app
-# import time
+from app.celery import celery_app
+import time
 
 
-# @celery_app.task
-# def test_task():
-#     time.sleep(2)
-#     print("Task completed")
+@celery_app.task
+def test_task():
+    time.sleep(2)
+    print("Task completed")
