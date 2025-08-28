4a9ca6390d2c063f52d4e6e1b904693d2a9852ea
Nick Joyce <actan-spb@mail.ru>
2025-08-28 13:25:06 +0300

    -

--- app/celery.py
+++ app/celery.py
@@ -1,16 +1,8 @@
 from celery import Celery
 
 # celery init
-celery_app = Celery("celery_worker")
-
-
-
-
+celery_app = Celery("celery_worker", broker_url="redis://127.0.0.1:6379/0", result_backend="redis://127.0.0.1:6379/0",)
 celery_app.conf.update(
-    broker_url="redis://127.0.0.1:6379/0",
-    result_backend="redis://127.0.0.1:6379/0",
-    # broker_use_ssl=ssl_options,
-    # redis_backend_use_ssl=ssl_options,
     task_serializer='json',
     result_serializer='json',
     accept_content=['json'],
