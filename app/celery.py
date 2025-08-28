796c061d923c703682c89f7d623456f304a44687
Nick Joyce <actan-spb@mail.ru>
2025-08-28 13:20:54 +0300

    -

--- app/celery.py
+++ app/celery.py
@@ -2,5 +2,23 @@ from celery import Celery
 
 # celery init
 celery_app = Celery("celery_worker")
-celery_app.config_from_object("settings", namespace="CELERY")
+
+
+
+
+celery_app.conf.update(
+    broker_url=CELERY_BROKER_URL,
+    result_backend=CELERY_RESULT_BACKEND,
+    # broker_use_ssl=ssl_options,
+    # redis_backend_use_ssl=ssl_options,
+    task_serializer='json',
+    result_serializer='json',
+    accept_content=['json'],
+    enable_utc=True,  # Убедитесь, что UTC включен
+    timezone='Europe/Moscow',  # Устанавливаем московское время
+    broker_connection_retry_on_startup=True,
+    task_acks_late=True,
+    task_reject_on_worker_lost=True,
+)
+
 celery_app.autodiscover_tasks()
