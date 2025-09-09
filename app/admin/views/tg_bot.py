690dc429f68cb55f749929744d0cf9dddd4d28be
Nick Joyce <actan-spb@mail.ru>
2025-09-09 16:58:37 +0300

    -

--- app/admin/views/tg_bot.py
+++ app/admin/views/tg_bot.py
@@ -226,6 +226,6 @@ class ChannelPostView(ModelView):
         StringField("caption", label="Подпись под картинкой"),
         StringField("chat_id", label="ID чата"),
         IntegerField("message_id", label="ID сообщения"),
-    ]]
+    ]
     exclude_fields_from_create = ["id", "created_at", "chat_id", "message_id"]
     exclude_fields_from_edit = ["id", "created_at", "chat_id", "message_id"]
