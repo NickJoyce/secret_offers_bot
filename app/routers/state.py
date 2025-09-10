1b8c56743ab89942d6af6c59e78251b3ee1728fa
Nick Joyce <actan-spb@mail.ru>
2025-09-10 11:31:27 +0300

    -

--- app/routers/state.py
+++ app/routers/state.py
@@ -47,60 +47,36 @@ async def is_subscriber(user_id: str, request: Request):
 @router.get("/manage-channel-post", include_in_schema=False)
 async def manage_channel_post(request: Request):
     try:
-        
-    
         chat_id = '-1003007138318'
         message_id = 80
-
 
         last_channel_post = await get_last_channel_post()
         
         photo_id = last_channel_post.photo['file_id']
         photo_path = f"{BASE_DIR}/app/uploads/attachment/{photo_id}"
 
-        
-        
-
-
-
-
-        # Редактируем сообщение
-        # await bot.edit_message_caption(
-        #     chat_id=chat_id,
-        #     message_id=message_id,
-        #     caption='🎲Играем по-новым правиламм!\nВсе прошлые акции уже ушли в историю — но на их место пришло нечто особенное ✨\n\nУ тебя есть всего 7 дней, чтобы поймать новый бонус!\n\n📍Место действия: Москва, пр-т Вернадского 41, стр. 1\n🗓Срок: с 01.09 по 07.09\nА вот и список «бонусов» 👇\n\n💎BBL Forever Clear (лицо) — 9 540 ₽ вместо 15 900 ₽\n💎BBL Forever Young (лицо) — 17 940 ₽ вместо 29 900 ₽\n💎BBL Skin Tyte (лицо) — 11 940 ₽ вместо 19 900 ₽\n💎BBL Удаление пигментации и сосудов (лицо) — 10 140 ₽ вместо 16 900 ₽\n💎BBL Усиленная программа (лицо) — 14 940 ₽ вместо 24 900 ₽\n\n⏳Время пошло — осталось всего несколько дней, чтобы успеть.\n👇Жми на кнопку, выбирай свою процедуру и забирай скидку!',
-        #     reply_markup= post_keyboard
-        # )
-       
-       
-        # photo_path = f"{BASE_DIR}/app/uploads/attachment/222.jpg"
-        # photo_file = FSInputFile(photo_path)
-       
         # отправляем сообщение c фото
         message = await bot.send_photo(chat_id=chat_id,
-                                        photo=FSInputFile(photo_path),
-                                        caption='![👍](tg://emoji?id=5368324170671202286)'+escape_markdown_v2(last_channel_post.caption),
-                                        reply_markup=post_keyboard, 
-                                        disable_notification=True,
-                                        parse_mode=ParseModes.MARKDOWN_V2)
+                                       photo=FSInputFile(photo_path),
+                                       caption=escape_markdown_v2(last_channel_post.caption),
+                                       reply_markup=post_keyboard,
+                                       parse_mode=ParseModes.MARKDOWN_V2)
         message_id = message.message_id
         
         last_channel_post.message_id = message_id
         last_channel_post.chat_id = chat_id
         await update_channel_post(last_channel_post)
         
+    
         
+        await sleep(3)
         
-        
-        # await sleep(3)
-        
-        # # редактируем сообщение: удаляем клавиатуру
-        # await bot.edit_message_caption(
-        #     chat_id=chat_id,
-        #     message_id=message_id,
-        #     caption=message_body,
-        #     reply_markup= None
-        # )
+        # редактируем сообщение: удаляем клавиатуру
+        await bot.edit_message_caption(
+            chat_id=chat_id,
+            message_id=message_id,
+            reply_markup= None
+        )
 
     
         return JSONResponse({"message_id": message_id})
