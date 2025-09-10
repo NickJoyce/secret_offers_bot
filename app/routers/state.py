46d678f8159219693a27c5676b56e26d8707009d
Nick Joyce <actan-spb@mail.ru>
2025-09-10 16:53:21 +0300

    -

--- app/routers/state.py
+++ app/routers/state.py
@@ -97,10 +97,15 @@ async def link_gen(request: Request):
             tags = f'?source=secretoffersbot&client_id={client.tg_id}'
 
             
-            link_1 = await bot.create_chat_invite_link(chat_id=chat_id, expire_date=expire_date, member_limit=1)
-            link_2 = await bot.create_chat_invite_link(chat_id=chat_id, expire_date=expire_date, member_limit=1)
+            link_1 = await bot.create_chat_invite_link(chat_id=chat_id, expire_date=expire_date, member_limit=1, creates_join_request=True)
+            link_2 = await bot.create_chat_invite_link(chat_id=chat_id, expire_date=expire_date, member_limit=1, creates_join_request=True)
             link_1 = link_1.invite_link + tags
             link_2 = link_2.invite_link + tags
+            
+            
+            
+            
+            
 
             
             
