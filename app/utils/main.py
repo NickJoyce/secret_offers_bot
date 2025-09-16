b2c99c454ae33ec30ab1c323d75e58b75fcce226
Nick Joyce <actan-spb@mail.ru>
2025-09-16 11:22:36 +0300

    -

--- app/utils/main.py
+++ app/utils/main.py
@@ -25,7 +25,6 @@ async def get_unique_promocode(length):
         promocode = generate_promocode(length=10)
         # проверяем есть ли промокод в базе
         existing = await get_promocode_by_value(promocode)
-        logger.info(f"existing: {existing}")
         # если промокода нет в базе, то выходим из цикла
         if not existing:
             break
