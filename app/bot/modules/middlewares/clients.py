ca938a77d20a91b9639bc34b464d44b4f0ea2bee
Nick Joyce <actan-spb@mail.ru>
2025-11-05 16:05:59 +0300

    -

--- app/bot/modules/middlewares/clients.py
+++ app/bot/modules/middlewares/clients.py
@@ -34,30 +34,30 @@ class AuthMiddleware(BaseMiddleware):
         return result
 
 
-# class BlackListMiddleware(BaseMiddleware):
-#         async def __call__(self,
-#                        handler: Callable[ [TelegramObject, Dict[str, Any]], Awaitable[Any] ],
-#                        event: TelegramObject,
-#                        data: Dict[str, Any]) -> Any:
-#             # Логика, которая выполняется ДО вызова обработчика
-#             logger.info(f"--- Black List Middleware: Processing event of type {type(event).__name__} ---")
-#             black_list = await get_black_list()
-#             ids = [user.tg_id for user in black_list if user.tg_id is not None]
-#             usernames = [user.tg_username for user in black_list if user.tg_username is not None]
+class BlackListMiddleware(BaseMiddleware):
+        async def __call__(self,
+                       handler: Callable[ [TelegramObject, Dict[str, Any]], Awaitable[Any] ],
+                       event: TelegramObject,
+                       data: Dict[str, Any]) -> Any:
+            # Логика, которая выполняется ДО вызова обработчика
+            logger.info(f"--- Black List Middleware: Processing event of type {type(event).__name__} ---")
+            # black_list = await get_black_list()
+            # ids = [user.tg_id for user in black_list if user.tg_id is not None]
+            # usernames = [user.tg_username for user in black_list if user.tg_username is not None]
             
-#             if event.from_user.id in ids or event.from_user.username in usernames:
-#                 # Если пользователь не разрешен, отправляем сообщение и прерываем цепочку
-#                 if isinstance(event, Message):
-#                     await event.reply(f"Ошибка подключения к боту (error code: blmw)")
-#                 elif isinstance(event, CallbackQuery):
-#                     await event.answer(f"Ошибка подключения к боту (error code: blmw)", show_alert=True)
-#                 return # Прерываем дальнейшую обработку 
+            # if event.from_user.id in ids or event.from_user.username in usernames:
+            #     # Если пользователь не разрешен, отправляем сообщение и прерываем цепочку
+            #     if isinstance(event, Message):
+            #         await event.reply(f"Ошибка подключения к боту (error code: blmw)")
+            #     elif isinstance(event, CallbackQuery):
+            #         await event.answer(f"Ошибка подключения к боту (error code: blmw)", show_alert=True)
+            #     return # Прерываем дальнейшую обработку 
             
-#             # Вызываем следующий обработчик в цепочке (это может быть другой middleware или конечный хендлер)
-#             result = await handler(event, data)
-#             # Логика, которая выполняется ПОСЛЕ вызова обработчика
-#             logger.info(f"--- Black List Middleware: Finished processing event of type {type(event).__name__} ---")
-#             return result               
+            # Вызываем следующий обработчик в цепочке (это может быть другой middleware или конечный хендлер)
+            result = await handler(event, data)
+            # Логика, которая выполняется ПОСЛЕ вызова обработчика
+            logger.info(f"--- Black List Middleware: Finished processing event of type {type(event).__name__} ---")
+            return result               
         
         
             
