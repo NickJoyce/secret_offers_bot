ebe44bdabc68baf0793e3c8a7712224bc9638a8b
Nick Joyce <actan-spb@mail.ru>
2025-11-06 16:20:07 +0300

    -

--- app/bot/modules/handlers/managers.py
+++ app/bot/modules/handlers/managers.py
@@ -94,13 +94,13 @@ async def process_post_data(message: types.Message, state: FSMContext, ):
         
         entities = message.entities
         
-        custom_emoji_id = []
+        custom_emoji_ids = []
         for entity in entities:
             if entity.type == 'custom_emoji':
-                custom_emoji_id.append(entity.custom_emoji_id)
+                custom_emoji_ids.append(entity.custom_emoji_id)
                 
         
-        await message.answer(f"{text}", entities=entities)
+        await message.answer(f"{text} {custom_emoji_ids}", entities=entities)
         await state.clear()
         return
     
