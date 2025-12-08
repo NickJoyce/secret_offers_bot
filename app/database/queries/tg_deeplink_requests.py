730781e84a78ccf6932d01885c01ec13c58f67aa
Nick Joyce <actan-spb@mail.ru>
2025-12-08 14:22:15 +0300

    -

--- app/database/queries/tg_deeplink_requests.py
+++ app/database/queries/tg_deeplink_requests.py
@@ -4,7 +4,7 @@ from app.database.models.tg_bot import DeeplinkRequest
 from sqlalchemy import select, update, delete, insert
 
 
-async def create_deeplink_request(items: list[dict]):
+def create_deeplink_request(items: list[dict]):
     with SyncSession.begin() as session:
         session.execute(insert(DeeplinkRequest), items)
 
