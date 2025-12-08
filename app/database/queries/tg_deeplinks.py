6d6521e16a99f87edc8f2fdc09563e2431b0867a
Nick Joyce <actan-spb@mail.ru>
2025-12-08 12:43:31 +0300

    -

--- app/database/queries/tg_deeplinks.py
+++ app/database/queries/tg_deeplinks.py
@@ -1,11 +1,11 @@
 from app.database.conn import AsyncSessionLocal
-from app.database.models.tg_bot import Deeplink
+from app.database.models.tg_bot import DeepLink
 from sqlalchemy import select, update, delete, insert
 
 
 async def get_deeplink(id_):
     async with AsyncSessionLocal() as session:
-        deeplink = await session.scalar(select(Deeplink).where(Deeplink.id == id_))
+        deeplink = await session.scalar(select(DeepLink).where(DeepLink.id == id_))
         return deeplink
 
 
