d4d06880088e1c7ebd1ae17365a07116d9ae37ab
Nick Joyce <actan-spb@mail.ru>
2025-12-12 15:40:32 +0300

    -

--- app/database/queries/tg_deeplink_requests.py
+++ app/database/queries/tg_deeplink_requests.py
@@ -10,6 +10,8 @@ from sqlalchemy import select, update, delete, insert
 async def acreate_deeplink_request(items: list[dict]) -> None:
     async with AsyncSessionLocal() as session:
         await session.execute(insert(DeeplinkRequest), items)
+        await session.commit()
+
 
 
 
