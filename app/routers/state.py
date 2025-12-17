9b4747ce71e1b88dbfdb6010ed2e70e6f8350866
Nick Joyce <actan-spb@mail.ru>
2025-12-17 11:19:15 +0300

    -

--- app/routers/state.py
+++ app/routers/state.py
@@ -50,7 +50,7 @@ html = """
         <ul id='messages'>
         </ul>
         <script>
-            var ws = new WebSocket("ws://localhost:8080/ws");
+            var ws = new WebSocket("ws://localhost:8000/ws");
             ws.onmessage = function(event) {
                 var messages = document.getElementById('messages')
                 var message = document.createElement('li')
