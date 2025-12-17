71ff55e28048b74bf2a57ab01870305f79e8d777
Nick Joyce <actan-spb@mail.ru>
2025-12-17 11:22:44 +0300

    -

--- app/routers/state.py
+++ app/routers/state.py
@@ -22,8 +22,7 @@ import string
 from app.utils.main import get_unique_promocode
 from fastapi.responses import RedirectResponse
 from app.database.queries.balck_list import get_black_list
-from fastapi import WebSocket
-from fastapi.responses import HTMLResponse
+
 
 
 logger = logging.getLogger(__name__)
@@ -33,53 +32,7 @@ router = APIRouter()
 templates = Jinja2Templates(directory=TEMPLATES_DIR)
 
 
-
-
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            var ws = new WebSocket("ws://localhost:8000/ws");
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
-</html>
-"""
-
-
-@router.get("/test_ws")
-async def get():
-    return HTMLResponse(html)
-
-@router.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")       
+           
 
 
 @router.get("/test", include_in_schema=False)
