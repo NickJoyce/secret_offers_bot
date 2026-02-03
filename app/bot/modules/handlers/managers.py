923839efe185445c62b2465cba9ace9a02da9fdb
Nick Joyce <actan-spb@mail.ru>
2026-02-03 14:52:01 +0300

    -

--- app/bot/modules/handlers/managers.py
+++ app/bot/modules/handlers/managers.py
@@ -103,7 +103,7 @@ async def download_clients_db(callback: CallbackQuery):
     clients = await get_clients()
     await callback.answer(f'clients: {len(clients)}')
     # создаем файл excel с данными о клиентах
-    df = pd.DataFrame(clients)
+    df = pd.DataFrame(clients.model_dump())
     df.to_excel(f"{BASE_DIR}/app/uploads/attachment/clients.xlsx", index=False)
     await callback.message.answer(text=f"Выгрузка клиентов создана")
     # отправляем файл пользователю
