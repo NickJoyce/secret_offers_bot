b3b91de96f746f4d6380b461d2d17ffd5aa15cbd
Nick Joyce <actan-spb@mail.ru>
2026-04-10 17:24:16 +0300

    -

--- app/bot/main.py
+++ app/bot/main.py
@@ -15,9 +15,10 @@ logging.config.dictConfig(LOGGING)
 logger = logging.getLogger(__name__)
 
 
-session = AiohttpSession(proxy=HTTP_PROXY)
+# session = AiohttpSession(proxy=HTTP_PROXY)
+# bot = Bot(token=TG_BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.MARKDOWN), session=session)
 
-bot = Bot(token=TG_BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.MARKDOWN), session=session)
+bot = Bot(token=TG_BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.MARKDOWN))
 dp = Dispatcher()
 
 
